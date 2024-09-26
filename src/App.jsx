0344b07f9df8a05a750e7f38ac161ad8ce60e22e
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-26 15:12:29 +0330

    added initial design of contact section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
