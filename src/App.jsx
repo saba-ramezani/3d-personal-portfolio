12856eea7ccdf0d8c776021fec0a8905789fd9be
saba-ramezani <saba.rad.sr@gmail.com>
2024-10-03 14:31:37 +0330

    added repo name to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"/saba-ramezani.github.io/"}>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
